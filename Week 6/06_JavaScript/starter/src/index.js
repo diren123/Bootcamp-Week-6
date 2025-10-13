5e42b54600a2cf9352b114c8901b9eca0f7ebb05
Diren Gunasekera <87416762+diren123@users.noreply.github.com>
2025-10-13 17:04:43 +0100

    06

--- Week 6/06_JavaScript/starter/src/index.js
+++ Week 6/06_JavaScript/starter/src/index.js
@@ -26,3 +26,23 @@ const movies = [
   }
 ];
 
+
+// Part 1: Function to find and log movie details
+function findMovie(movieTitle) {
+  console.log("Searching for movie:", movieTitle);
+
+  for (const movie of movies) {
+    if (movie.title === movieTitle) {
+      console.log("Movie found!");
+      console.log("Title:", movie.title);
+      console.log("Director:", movie.director);
+      console.log("Year:", movie.year);
+    }
+  }
+
+  console.log("Search complete.");
+}
+
+findMovie("Star Wars");
+findMovie("Thor: Ragnarok"); // This will not find a match
+
