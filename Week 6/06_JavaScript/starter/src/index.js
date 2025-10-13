76455649d5c1b68229b9fb6e2ed282db208dc8f2
Diren Gunasekera <87416762+diren123@users.noreply.github.com>
2025-10-13 16:31:29 +0100

    06

---
+++ Week 6/06_JavaScript/starter/src/index.js
@@ -0,0 +1,28 @@
+const movies = [
+  {
+    title: "Jaws",
+    director: "Steven Spielberg",
+    year: "1975"
+  },
+  {
+    title: "Star Wars",
+    director: "George Lucas",
+    year: "1977"
+  },
+  {
+    title: "Avengers: Infinity War",
+    director: "Anthony and Joe Russo",
+    year: "2018"
+  },
+  {
+    title: "Top Gun",
+    director: "Tony Scott",
+    year: "1986"
+  },
+  {
+    title: "Justice League",
+    director: "Zack Snyder",
+    year: "2017"
+  }
+];
+
